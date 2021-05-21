9133c6bedc2026638606e376263c14de2c7319cb
Parth <dkm062@gmail.com>
2021-05-21 14:18:29 +0530

    reset password added .

--- device/auth/resetPassword.php
+++ device/auth/resetPassword.php
@@ -1,9 +1,47 @@
 <?php     
-	// ini_set("SMTP","smtp.gmail.com");
-	// ini_set("smtp_port","465");
-	$to_email = 'dkm062@gmail.com';
-	$subject = 'Testing PHP Mail';
-	$message = 'This mail is sent using the PHP mail function';
-	$headers = 'From:meplusblack@gmail.com';
-	mail($to_email,$subject,$message,$headers);
+require_once($_SERVER['DOCUMENT_ROOT']."/VLT-backend/database/DAO.php");
+
+function generateRandomString($length = 10) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
+$dao = new DAO();
+$response = new Response();
+
+$json = file_get_contents('php://input');
+$request = json_decode($json);
+
+$email = $request->email;
+
+$user = $dao->get("User", $email, "email");
+if( is_null($user->email) ) 
+{
+    $response->status = 0; 
+}else{
+	$newPass = generateRandomString();
+    $user->password = $newPass;
+	$to_email = $user->email;
+	$subject = 'New password for VLT lanudry';
+	$message = 'Your new password for the VLT lanudry account is '.$newPass;
+	$headers = 'From:info@volt.com';
+	$isSent = mail($to_email,$subject,$message,$headers);
+	if($isSent){
+    	$dao->update($user);
+    	$response->status = 1; 
+	}else{
+    	$response->status = 2; 
+	}
+}    
+$dao->close();
+echo  json_encode($response); 
+
+class Response {
+    public $status;
+}
+
 ?>
